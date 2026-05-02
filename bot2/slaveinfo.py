7576bd87ad04db6f589934d74fce9d3b62d2379b
mattip <matti.picus@gmail.com>
2026-05-02 21:33:51 +0300

    add defaults

--- bot2/slaveinfo.py
+++ bot2/slaveinfo.py
@@ -1,6 +1,6 @@
 
 # Mapping from slave name to slave password
-passwords = {}
+passwords = {'localhost': 'localhost'}
 
 # Credentials for uploading benchmark results to speed.pypy.org
 upload_credentials = {'username': '', 'password': ''}
